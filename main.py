427299496e94485a2c029cb725cccacdcf03ebf3
marcoassilva70 <marcoantoniodossantossilva70@gmail.com>
2026-06-21 21:52:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(
+    title="Aula",
+    description="Aula FastAPI",  # noqa: F821
+    summary="API desenvolvida durante a aula de Construção de APIs para IA",
+    version="0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+    "name": "Marco Antonio S. Silva",
+    "url": "http://github.com/marcoassilva70/",
+    "email": "marcoantoniodossantossilva70@gmail.com",
+    },
+    license_info={
+    "name": "Apache 2.0",
+    "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
 
 class Numeros(BaseModel):
     numero1: int
     numero2: int
+
+class Resultado(BaseModel):
+    resultado: int
 
 @app.get("/teste")
 def hello_world():
@@ -24,6 +42,6 @@ def soma_formato2(numero1: int, numero2: int):
     return {"resultado": total}
 
 @app.post("/soma_formato3")
-def soma_formato3(numeros: Numeros):
+def soma_formato3(numeros: Numeros) -> Resultado:
     total = numeros.numero1 + numeros.numero2
     return {"resultado": total}
